86e2782e5676ad521320ba4bb5f7376d1163560f
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2019-11-06 11:12:47 +0900

    adjust directory to ubuntu style

--- scripts/fetch_nvim.sh
+++ scripts/fetch_nvim.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mkdir -p ~/bin
-wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -O ~/bin/vim
-chmod +x ~/bin/vim
+mkdir -p ~/local/bin
+wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -O ~/local/bin/nvim
+chmod +x ~/local/bin/nvim
