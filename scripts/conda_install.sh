6b2e33ae661117be7e581b9c7e86b7c4331489bf
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2020-03-03 07:03:39 +0900

    powerlevel10k

---
+++ scripts/conda_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Prerequisites
+#sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh -b
