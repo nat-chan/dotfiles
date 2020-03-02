48eb82ad0714a72a09e0b27cba47aa6a0724f2ae
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2020-03-02 12:30:29 +0000

    pp

---
+++ scripts/pp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PORTA=22
+PORTB=10022
+#PIPEA=ppng.ml/3f786850e387550fdab836ed7e6dc881de23001b
+#PIPEB=ppng.ml/89e6c98d92887913cadf06b2adb97f26cde4849b
+PIPEA=18.176.57.176/3f786850e387550fdab836ed7e6dc881de23001b
+PIPEB=18.176.57.176/89e6c98d92887913cadf06b2adb97f26cde4849b
+
+if [ $1 = 'server' ]; then
+    socat "EXEC:curl -NsS http\://${PIPEA}!!EXEC:curl -NsST - http\://${PIPEB}" TCP:127.0.0.1:${PORTA}
+elif [ $1 = 'client' ];then
+    socat TCP-LISTEN:${PORTB} "EXEC:curl -NsS http\://${PIPEB}!!EXEC:curl -NsST - http\://${PIPEA}"
+elif [ $1 = 'deploy' ];then
+    wget https://github.com/nwtgck/piping-server-pkg/releases/download/v0.15.8/piping-server-linux
+    chmod +x piping-server-linux
+    sudo ./piping-server-linux --http-port=80
+else
+    exit 255
+fi
+
