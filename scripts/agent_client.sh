6521b193c0fbd284b85a34610193c7aa30c08adf
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2020-04-14 16:17:29 +0900

    spilit agent and agent_client

---
+++ scripts/agent_client.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/zsh
+
+if ! killall -0 ssh-agent 2> /dev/null ;then
+    eval `ssh-agent|tee ~/.ssh/agent`
+#    ssh-add ~/.ssh/id_rsa ~/.ssh/id_rsa.git ~/.ssh/id_rsa.hpcs
+    ssh-add ~/.ssh/id_rsa*
+else
+    source ~/.ssh/agent > /dev/null
+    echo -ne "[ $SSH_AGENT_PID:$SSH_AUTH_SOCK ]"
+    ssh-add -l| while read n sha key rsa; do
+        echo -ne " `basename $key`"
+    done
+    echo
+fi
--- scripts/conda_install.sh
+++ scripts/conda_install.sh
@@ -4,3 +4,4 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh -b
+rm ./Miniconda3-latest-Linux-x86_64.sh
